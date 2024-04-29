e81ffab70b8c23e7f21127735afd52e227276933
pureveg <153623346+pureveg@users.noreply.github.com>
2024-04-29 17:15:43 +0530

    uncomment

--- 59.Write a program using PIPE, to convert uppercase to lowercase filter to read command from file.c
+++ 59.Write a program using PIPE, to convert uppercase to lowercase filter to read command from file.c
@@ -10,13 +10,11 @@ int main(int argc, char *argv[]) {
     pid_t pid;
     char buffer[BUFFER_SIZE];
 
-    // Create the pipe
     if (pipe(pipefd) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
 
-    // Fork a child process
     pid = fork();
 
     if (pid < 0) {
@@ -24,8 +22,8 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if (pid > 0) {  // Parent process (reader)
-        close(pipefd[0]); // Close the read end of the pipe in the parent
+    if (pid > 0) {  
+        close(pipefd[0]); 
 
         FILE *file;
         if (argc > 1) {
@@ -38,22 +36,19 @@ int main(int argc, char *argv[]) {
             file = stdin;
         }
 
-        // Read from the file or standard input and write to the pipe
         ssize_t bytes_read;
         while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
             write(pipefd[1], buffer, bytes_read);
         }
 
-        // Close the write end of the pipe in the parent
         close(pipefd[1]);
-        
+
         if (file != stdin) {
             fclose(file);
         }
-    } else { // Child process (converter)
-        close(pipefd[1]); // Close the write end of the pipe in the child
+    } else { 
+        close(pipefd[1]); 
 
-        // Read from the pipe, convert uppercase to lowercase, and write to standard output
         ssize_t bytes_read;
         while ((bytes_read = read(pipefd[0], buffer, BUFFER_SIZE)) > 0) {
             for (int i = 0; i < bytes_read; i++) {
@@ -62,19 +57,9 @@ int main(int argc, char *argv[]) {
             write(STDOUT_FILENO, buffer, bytes_read);
         }
 
-        // Close the read end of the pipe in the child
         close(pipefd[0]);
     }
 
     return 0;
 }
 
-
-
-
-
-
-
-//Execution cmd:
-//gcc your_program_name.c -o your_program_name
-//./your_program_name input_file.txt
