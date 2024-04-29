b49090d361ca32218c604cd46115a2e1c9e39730
pureveg <153623346+pureveg@users.noreply.github.com>
2024-04-29 16:55:27 +0530

    Uncommented

--- 56.Write a program using FIFO to Send data from parent to child over a pipe named pipe.c
+++ 56.Write a program using FIFO to Send data from parent to child over a pipe named pipe.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-#define BUFFER_SIZE 25
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define BUFFER_SIZE 25
+#define BUFFER_SIZE 40
 #define FIFO_PATH "/tmp/myfifo"
 
 int main() {
@@ -19,13 +14,11 @@ int main() {
     char buffer[BUFFER_SIZE];
     const char *message = "Hello child, this is your parent!";
 
-    // Create the FIFO (named pipe)
     if (mkfifo(FIFO_PATH, 0666) == -1) {
         perror("mkfifo");
         exit(EXIT_FAILURE);
     }
 
-    // Fork a child process
     pid = fork();
 
     if (pid < 0) {
@@ -33,38 +26,30 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    if (pid > 0) {  // Parent process
-        // Open the FIFO for writing
+    if (pid > 0) {  
+
         fd = open(FIFO_PATH, O_WRONLY);
         if (fd == -1) {
             perror("open");
             exit(EXIT_FAILURE);
         }
-        // Write message to the FIFO
+
         write(fd, message, BUFFER_SIZE);
         close(fd);
-    } else { // Child process
-        // Open the FIFO for reading
+    } else { 
+
         fd = open(FIFO_PATH, O_RDONLY);
         if (fd == -1) {
             perror("open");
             exit(EXIT_FAILURE);
         }
-        // Read message from the FIFO
+
         read(fd, buffer, BUFFER_SIZE);
         printf("Message from parent: %s\n", buffer);
         close(fd);
     }
 
-    // Remove the FIFO
     unlink(FIFO_PATH);
 
     return 0;
 }
-
-
-
-
-//Execution cmd
-//gcc your_program_name.c -o your_program_name
-//./your_program_name
